ebfe090a62ab5cb82590c70064ddec92c65b0a37
v1nc3ntl1 <v1nc3ntl1@gmail.com>
2015-05-25 15:28:14 +0800

    Include a ShoppingCart class

---
+++ WebApplication1/MvcApplication1/Models/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class ShoppingCart
+    {
+        private string _id = "";
+        private string _customerId;
+
+        public string Id
+        {
+            get { return _id;}
+            set { _id = value; }
+        }
+
+        public string CustomerId
+        {
+            get { return _customerId; }
+            set { _customerId = value; }
+        }
+    }
+}
